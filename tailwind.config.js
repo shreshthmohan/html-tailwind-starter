749b0c789a8348f0e5f3ed8117a21f2a4a5e576b
Shreshth Mohan <sremog@gmail.com>
2023-11-06 12:18:10 +0530

    code theme wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,58 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // TODO: the idea now is that you add an object with such keys below
+      // to override the default (and dark) theme for code blocks
+
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            code: { backgroundColor: "green" },
+            pre: { backgroundColor: "green" },
+            ".token": {
+              backgroundColor: "blue",
+              color: "white",
+            },
+          },
+        },
+        invert: {
+          css: {
+            "--tw-prose-body": theme("colors.red[900]"),
+            code: { backgroundColor: "red" },
+            ".token": {
+              backgroundColor: "blue",
+              color: "white",
+            },
+          },
+        },
+      }),
+      // typography: {
+      //   // https://github.dev/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
+      //   DEFAULT: {
+      //     css: {
+      //       "code::before": {
+      //         content: '""',
+      //       },
+      //       "code::after": {
+      //         content: '""',
+      //       },
+      //       h1: {
+      //         fontWeight: "600",
+      //       },
+      //       h2: {
+      //         fontWeight: "500",
+      //       },
+      //       h3: {
+      //         fontWeight: "500",
+      //       },
+      //       ".code-line": {
+      //         backgroundColor: "red",
+      //       },
+      //     },
+      //   },
+      // },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
